c64a0c0370773fde706dff1cb7cac2ec367c476c
DanielCarvalho93 <danielcarvalho@yahoo.co.uk>
2022-07-08 00:15:56 +0100

    soundchanges

--- your-code/workingmerge.py
+++ your-code/workingmerge.py
@@ -4,6 +4,9 @@ from pygame import mixer
 pygame.init()
 soundObj = mixer.Sound('SawTheme.ogg')
 soundObj.play()
+pygame.mixer.music.load('SawTheme.ogg')
+pygame.mixer.music.play(-1)
+
 # define rooms and items
 
 couch = {
@@ -255,6 +258,7 @@ def play_room(room):
     """
     game_state["current_room"] = room
     if(game_state["current_room"] == game_state["target_room"]):
+        pygame.mixer.music.stop()
         soundObj.stop()
         playsound('alive.mp3')
         print("Congrats! You escaped the room!")
